2fc4bd388c867df0a7f4d4aac04205e35fd1c2dc
Nestor <ncalderont2@miumg.edu.gt>
2024-08-23 18:52:34 -0600

    db_config

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,9 @@ import mysql from 'mysql';
 dotenv.config();
 
 export const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
 });
